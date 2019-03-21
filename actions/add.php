cf57004887e19ae9051eb1e3a2d31006e346103b
Alex Fix <dasdias@yandex.ru>
2019-03-21 14:56:51 +0300

    day two. intensive PHP

---
+++ actions/add.php
@@ -0,0 +1,46 @@
+<?php
+session_start(); 
+require_once '../db/db.php';
+
+// echo('<pre>');
+// var_dump($_POST);
+// echo('</pre>');
+
+ // unset($_SESSION['totalQuantity']);
+ // unset($_SESSION['totalPrice']);
+ // unset($_SESSION['cart']);
+
+if(isset($_POST['id'])) {
+
+
+
+
+  $id = $_POST['id'];
+  $product = $connect->query("SELECT * FROM products WHERE id='$id'");
+  $product = $product->fetch(PDO::FETCH_ASSOC);
+
+
+
+
+  if(isset($_SESSION['cart'][$id])) {
+    $_SESSION['cart'][$id]['quantity'] += 1;
+  }
+  else {
+    $_SESSION['cart'][$id] = [
+      'title' => $product['title'],
+      'price' => $product['price'],
+      'rus_name' => $product['rus_name'],
+      'img' => $product['img'],
+      'quantity' => 1,
+    ];
+  }
+
+
+  $_SESSION['totalQuantity'] = @$_SESSION['totalQuantity'] ? $_SESSION['totalQuantity'] += 1 : 1; 
+  $_SESSION['totalPrice'] = @$_SESSION['totalPrice'] ? $_SESSION['totalPrice'] += $product['price'] : $product['price']; 
+// echo('<pre>');
+// var_dump($_SESSION);
+// echo('</pre>');
+}
+header('Location:' . $_SERVER['HTTP_REFERER']);
+?>
--- product.php
+++ product.php
@@ -1,12 +1,35 @@
 <?php 
-require_once 'parts/header.php'
+require_once 'parts/header.php';
+
+if (isset($_GET['product'])) {
+    $currentProduct = $_GET['product'];
+    $product = $connect->query("SELECT * FROM products WHERE title='$currentProduct'");
+    $product = $product->fetch(PDO::FETCH_ASSOC);
+}
+/*echo('<pre>');
+print_r();
+echo('</pre>');*/
+
+// echo('<pre>');
+// var_dump();
+// echo('</pre>');
+
 ?>
+<?php if (empty($product)) { ?>
+	<div class="main">
+		<div class="card">Такого товара не существует!</div>
+	</div>
+<?php } else { ?>
+	<div class="product-card">
+	    <a href="index.php">Вернуться на главную</a>
 
-<div class="product-card">
-    <a href="index.php">Вернуться на главную</a>
-
-    <h2>Товар (55 рублей)</h2>
-    <div class="descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, assumenda corporis deserunt dicta dolores ex illum inventore iusto nam nisi nobis obcaecati perspiciatis qui ratione recusandae similique sunt veritatis voluptatibus.</div>
-    <img width="300" src="img/amanita.jpg" alt="Фото">
-    <button type="submit">Добавить в корзину</button>
-</div>
+	    <h2><?=$product['rus_name']?> (<?=$product['price']?> рублей)</h2>
+	    <div class="descr"><?=$product['descr']?></div>
+	    <img width="300" src="img/<?=$product['img']?>" alt="<?=$product['rus_name']?>">
+	    <form method="POST" action="actions/add.php">
+            <input type="hidden" name="id" value="<?=$product['id']?>">
+            <input type="submit" value="Добавить в корзину">
+        </form>
+	    <!-- <button type="submit">Добавить в корзину</button> -->
+	</div>
+<?php } ?>
